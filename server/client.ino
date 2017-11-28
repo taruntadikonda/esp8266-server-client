278cad671274e8eb3c3d21ccd8092534284ac995
tarun <taditarun123@gmail.com>
2017-11-28 20:47:49 +0530

    Add files via upload

---
+++ server/client.ino
@@ -0,0 +1,59 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid     = "********";   //server ssid
+const char* password = "*********";  //server password
+const char* host = "http://192.168.4.1"; //deault ip address
+HTTPClient http;
+
+void rptaSrv(int httpCode){
+  if(httpCode == 200) {
+           String payload = http.getString();
+           Serial.println(payload);
+        } else {
+            Serial.print("[HTTP] GET... failed, no connection or no HTTP server\n");
+        }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("proyecto inv client-server");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);  //sin esta no funciona
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop(){
+if(WiFi.status() != WL_CONNECTED){
+  WiFi.begin(ssid, password);
+  delay(500);
+}else{
+  //HTTPClient http;
+        Serial.print("[HTTP] begin...\n");
+        
+        int a=http.begin("192.168.4.1", 80, "/data"); //pinging the url
+        int httpCode1 = http.GET();
+        Serial.println(a);
+        if(a==1)
+        {
+          //actions
+        }
+        else
+        {
+          //actions
+        }
+        
+        rptaSrv(httpCode1);
+        delay(1000);
+        }
+}
---
+++ server/server.ino
@@ -0,0 +1,39 @@
+#include<WiFiClient.h> 
+#include<ESP8266WebServer.h>`
+int value1;
+
+const char *ssid = "*******";     //intilize the server ssid
+const char *password = "********";  //password of server
+
+ESP8266WebServer server(80);
+int sensor()
+{
+  value1=digitalRead(13);
+  return value1;
+}
+void handle_data()  //handler function
+{
+  sensor();
+  digitalWrite(2, 1);
+  server.send(200, "text/plain", String(value1)); //sending data to url
+}
+
+
+void setup() {
+  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+  Serial.println("HTTP server started");
+  server.on("/data",handle_data);
+  
+}
+
+void loop() {
+  server.handleClient();
+}
